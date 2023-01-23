132887bbb27cbd6b2e6cd442f64f5e9a74322f65
fkakd1205 <fkakd0102@naver.com>
2023-01-23 17:10:33 +0900

    feat & refactor : 인가 필터 리팩토링 및 권한설정 테스트 TestController => 테스트 컨트롤러 생성 SecurityConfig => form login, http basic login disable 설정. 세션관리 해제. 테스트 컨트롤러 권한 설정 JwtAuthorizationFilter => 인가 필터 리팩토링. 인가 완료 후 security context holder에 인증 객체 저장

--- src/main/java/com/example/server/security/SecurityConfig.java
+++ src/main/java/com/example/server/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -13,6 +14,7 @@ import org.springframework.security.web.context.HttpSessionSecurityContextReposi
 import org.springframework.security.web.context.SecurityContextRepository;
 
 import com.example.server.domain.refresh_token.repository.RefreshTokenRepository;
+import com.example.server.domain.user.repository.UserRepository;
 import com.example.server.security.auth.CustomUserDetailsService;
 import com.example.server.security.auth.JwtAuthenticationFilter;
 import com.example.server.security.auth.JwtAuthenticationProvider;
@@ -26,6 +28,7 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
     private final CustomUserDetailsService customUserDetailsService;
     private final RefreshTokenRepository refreshTokenRepository;
+    private final UserRepository userRepository;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -35,28 +38,30 @@ public class SecurityConfig {
 
         http
             .cors().disable()
-            .csrf().disable();
-            // .formLogin().disable();
+            .csrf().disable()
+            .formLogin().disable()
+            .httpBasic().disable()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);    // jwt사용으로 세션관리 해제
+            // .usernameParameter("uesrname")      // form login에서 왜 uesrname으로 넘어올까..
+            // .passwordParameter("password")
+            // .permitAll()
+            // .loginProcessingUrl("/api/v1/login")
+            // .defaultSuccessUrl("/");
         
         http
             .headers().frameOptions().sameOrigin();
 
         http
-            .formLogin()
-            .usernameParameter("uesrname")      // form login에서 왜 uesrname으로 넘어올까..
-            .passwordParameter("password")
-            .permitAll()
-            .loginProcessingUrl("/api/v1/login")
-            .defaultSuccessUrl("/");
-
-        http
-            .authorizeRequests(request -> request
-                .antMatchers(
-                    "/api/v1/home",
-                    "/api/v1/login",
-                    "/api/v1/signup"
-                ).permitAll()
-                .anyRequest().authenticated()
+            .authorizeRequests((request) -> 
+                request
+                    .antMatchers("/api/v1/test/**").hasAuthority("ROLE_USER")
+                    .antMatchers(
+                        "/api/v1/home",
+                        "/api/v1/login",
+                        "/api/v1/signup"
+                    ).permitAll()
+                    .anyRequest().authenticated()
             );
 
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
@@ -92,7 +97,7 @@ public class SecurityConfig {
 
     @Bean
     public JwtAuthorizationFilter jwtAuthorizationFilter() {
-        JwtAuthorizationFilter authorizationFilter = new JwtAuthorizationFilter(refreshTokenRepository);
+        JwtAuthorizationFilter authorizationFilter = new JwtAuthorizationFilter(userRepository, refreshTokenRepository);
         return authorizationFilter;
     }
 }
--- src/main/java/com/example/server/security/auth/JwtAuthorizationFilter.java
+++ src/main/java/com/example/server/security/auth/JwtAuthorizationFilter.java
@@ -1,6 +1,7 @@
 package com.example.server.security.auth;
 
 import java.io.IOException;
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.Arrays;
 import java.util.Optional;
 import java.util.UUID;
@@ -12,28 +13,35 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.example.server.domain.message.Message;
 import com.example.server.domain.refresh_token.entity.RefreshToken;
 import com.example.server.domain.refresh_token.repository.RefreshTokenRepository;
+import com.example.server.domain.user.entity.User;
+import com.example.server.domain.user.repository.UserRepository;
 import com.example.server.security.properties.AuthTokenProperties;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.example.server.security.service.JwtAuthService;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
+    private UserRepository userRepository;
     private RefreshTokenRepository refreshTokenRepository;
 
-    public JwtAuthorizationFilter(RefreshTokenRepository refreshTokenRepository) {
+    public JwtAuthorizationFilter(UserRepository userRepository, RefreshTokenRepository refreshTokenRepository) {
+        this.userRepository = userRepository;
         this.refreshTokenRepository = refreshTokenRepository;
     }
 
+    // 1. 액세스토큰을 담고 있는 쿠키를 확인
+    // 2. 액세스토큰이 유효하다면 -> 인증된 객체 저장하고 doFilter, 그렇지 않다면 -> 리프레스토큰 검사
+    // 3. DB에서 리프레시토큰 조회. 리프레시 토큰이 유효하다면 -> 새로운 액세스토큰 발급, 그렇지 않다면 -> 인증된 객체를 저장하지 않고 doFilter
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
@@ -54,16 +62,22 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
             System.out.println("-----access token expired-----");
             claims = e.getClaims();
             isAccessTokenExpired = true;
-        } catch (Exception e) {
-            // TODO
+        } catch (Exception e) {     // TODO :: 구체 예외 처리
+            filterChain.doFilter(request, response);
+            return;
         }
 
-        // TODO :: 리프레시 토큰 만료된 경우 처리
+        Optional<User> userOpt = userRepository.findByUsername(claims.get("username").toString());
+
         // 액세스토큰이 만료된 경우
         if(isAccessTokenExpired) {
             UUID refreshTokenId = UUID.fromString(claims.get("refreshTokenId").toString());
             Optional<RefreshToken> refreshTokenOpt = refreshTokenRepository.findById(refreshTokenId);
-            
+
+            if(!userOpt.isPresent()) {
+                throw new UserPrincipalNotFoundException("not found user.");
+            }
+            // 액세스토큰과 매칭된 리프레시토큰을 DB에서 조회한다
             if(refreshTokenOpt.isPresent()) {
                 Claims refreshTokenClaims = null;
                 String refreshToken = refreshTokenOpt.get().getRefreshToken();
@@ -73,14 +87,21 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
                 } catch (ExpiredJwtException e) {
                     // 리프레시 토큰이 만료된 경우
                     System.out.println("-----refresh token expired-----");
+                    
+                    // 만료된 리프레시 토큰을 제거 후 doFitler
                     refreshTokenRepository.delete(refreshTokenOpt.get());
-                } catch (Exception e) {
-                    // TODO
+                    filterChain.doFilter(request, response);
+                    return;
+                } catch (Exception e) {     // TODO :: 구체 예외 처리
+                    filterChain.doFilter(request, response);
+                    return;
                 }
 
                 // 리프레시 토큰이 존재한다면 액세스토큰 발급
+                String newAccessToken = JwtAuthService.createAccessToken(userOpt.get(), refreshTokenId);
+
                 if(refreshTokenClaims != null) {
-                    ResponseCookie cookies = ResponseCookie.from("access_token", accessToken)
+                    ResponseCookie cookies = ResponseCookie.from("access_token", newAccessToken)
                         .httpOnly(true)
                         .domain("localhost")
                         // .secure(true)
@@ -90,22 +111,25 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
                         .build();
 
                     response.addHeader(HttpHeaders.SET_COOKIE, cookies.toString());
-
-                    // 로그인 성공 response 세팅
-                    Message message = new Message();
-                    message.setStatus(HttpStatus.OK);
-                    message.setMessage("success");
-                    message.setMemo("login_success");
-
-                    response.setStatus(HttpStatus.OK.value());
-                    response.setContentType(MediaType.APPLICATION_JSON.toString());    
-                    new ObjectMapper().writeValue(response.getOutputStream(), message);
                 }
             }else {
+                System.out.println("-----not found refresh token-----");
+                
                 // TODO :: 인가 거절
+                filterChain.doFilter(request, response);
+                return;
             }
         }
 
-        // filterChain.doFilter(request, response);
+        this.saveAuthenticationToSecurityContextHolder(userOpt.get());
+        filterChain.doFilter(request, response);
+    }
+
+    private void saveAuthenticationToSecurityContextHolder(User user) {
+        CustomUserDetails userDetails = new CustomUserDetails(user);
+
+        // 인가 처리가 정상적으로 완료된다면 Authentication 객체 생성
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 }
